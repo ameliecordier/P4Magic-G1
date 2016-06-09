b2e9ee55f095de365bea4bad4448ec9056addf00
Dorian Wouters <elementw@openmailbox.org>
2016-06-09 08:56:38 +0200

    Implémentation de ColumnDisappearEffect

--- src/model/ColumnDisappearEffect.java
+++ src/model/ColumnDisappearEffect.java
@@ -6,9 +6,20 @@ package model;
  */
 public class ColumnDisappearEffect extends Effect {
 
+    /**
+     * This effect removes all the tokens on the board column the effect tile
+     * is on, including the token just played.
+     *
+     * @param line
+     * @param column
+     * @param game
+     */
     @Override
     public void playEffect(int line, int column, Game game) {
-        throw new UnsupportedOperationException("NYI");
+        final int height = game.getBoard().getHeight();
+        for (int i = height - 1; i >= 0; --i) {
+            game.getBoard().getTileIJ(i, column).setStatus(-1);
+        }
     }
 
 }
