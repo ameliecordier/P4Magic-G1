e11472fcf8203e7506732d76b0e013fd5c54e8ab
Dorian Wouters <elementw@openmailbox.org>
2016-06-09 08:30:09 +0200

    Création de DisappearEffect: code squelette et tests

---
+++ src/model/DisappearEffect.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ *
+ * @author ElementW
+ */
+public class DisappearEffect extends Effect {
+
+    public DisappearEffect() {
+        
+    }
+
+    @Override
+    public void playEffect(int line, int column, Game game) {
+        throw new UnsupportedOperationException("NYI");
+    }
+    
+}
