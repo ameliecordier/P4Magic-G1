8faf179cb358869bd25c3206944afb2c0d93199d
Pierre Friedrichs <hyacinthe@openmailbox.org>
2016-06-09 09:28:02 +0200

    Nouvel effet implémenté : RemoveLineEffect

---
+++ src/model/RemoveLineEffect.java
@@ -0,0 +1,39 @@
+/**
+ * MagicP4
+ * IUT Lyon 1 - 2016
+ */
+package model;
+
+/**
+ *
+ * @author hakkahi - IUT Lyon 1 - 2016
+ */
+public class RemoveLineEffect extends Effect {
+
+    /**
+     * This effect disappears the lile that has just been played and
+     * thus the owner of the tile)
+     *
+     * @param line
+     * @param column
+     * @param game
+     */
+    @Override
+    public void playEffect(int line, int column, Game game) {
+
+        int tile_id = game.getBoard().getTileIJ(line, column).getStatus();
+        int player1_id = game.getPlayer1().getId();
+        int player2_id = game.getPlayer2().getId();
+
+        if (tile_id == player1_id) {
+            for (int i = 0 ; i < game.getBoard().getWidth() ; i++) {
+                int ancienneValeur = game.getBoard().getTileIJ(line-1, i).getStatus();
+                game.getBoard().getTileIJ(line, i).setStatus(ancienneValeur);
+            }
+        } else {
+            tile_id = player1_id;
+        }
+        game.getBoard().getTileIJ(line, column).setStatus(tile_id);
+    }
+
+}
